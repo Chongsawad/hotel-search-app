ff754e2f76e0b8cb4474362627d7fc2642711a1b
Chongsawad Saiaram <chongsawad252@gmail.com>
2025-07-06 09:01:09 +0700

    Init

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function App() {
+  return (
+    <div style={{ padding: '2rem', fontFamily: 'sans-serif' }}>
+      <h1>Hello, React 👋</h1>
+      <p>This is a simple React app scaffold.</p>
+    </div>
+  );
+}
+
+export default App;
